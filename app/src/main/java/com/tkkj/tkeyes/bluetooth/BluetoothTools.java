6fb13b55eaccdc16d1f5aa760f90019ee476e238
TKKJ视训仪 <zhangyuanqiao0912@163.com>
2017-04-10 22:11:05 +0800

    send and receiver

---
+++ app/src/main/java/com/tkkj/tkeyes/bluetooth/BluetoothTools.java
@@ -0,0 +1,16 @@
+package com.tkkj.tkeyes.bluetooth;
+
+/**
+ * Created by TKKJ on 2017/4/10.
+ *
+ */
+
+public class BluetoothTools {
+
+
+    public void sendMessge(){
+
+    }
+
+
+}
--- app/src/main/java/com/tkkj/tkeyes/bluetoothutil/SecureConnector.java
+++ app/src/main/java/com/tkkj/tkeyes/bluetoothutil/SecureConnector.java
@@ -2,10 +2,18 @@ package com.tkkj.tkeyes.bluetoothutil;
 
 
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.inuker.bluetooth.library.Code;
 import com.inuker.bluetooth.library.connect.response.BleNotifyResponse;
+import com.inuker.bluetooth.library.connect.response.BleReadResponse;
 import com.inuker.bluetooth.library.connect.response.BleWriteResponse;
+import com.inuker.bluetooth.library.receiver.listener.BluetoothReceiverListener;
+import com.inuker.bluetooth.library.utils.BluetoothLog;
 import com.inuker.bluetooth.library.utils.ByteUtils;
 import com.inuker.bluetooth.library.utils.UUIDUtils;
 
@@ -18,14 +26,18 @@ import java.util.UUID;
 public class SecureConnector {
 
     private static BluetoothDevice mDevice;
+    private static final int serviceUuid = 0xFFE5 ;//0xFE95
+    private static final int charaUuid = 0xFFE9;//0x10,0xFFE
+    private static final int value = 0x90CA85DE;//0x90CA85DE,0xDE85CA90
 
     public static void processStep1(BluetoothDevice device) {
         mDevice = device;
 
-        ClientManager.getClient().write(device.getAddress(), UUIDUtils.makeUUID(0xFE95), UUIDUtils.makeUUID(0x10),
-                ByteUtils.fromInt(0xDE85CA90), new BleWriteResponse() {
+        ClientManager.getClient().write(device.getAddress(), UUIDUtils.makeUUID(serviceUuid), UUIDUtils.makeUUID(charaUuid),
+                ByteUtils.fromInt(value), new BleWriteResponse() {
                     @Override
                     public void onResponse(int code) {
+
                         if (code == Code.REQUEST_SUCCESS) {
                             processStep2();
                         }
@@ -34,16 +46,45 @@ public class SecureConnector {
     }
 
     public static void processStep2() {
-        ClientManager.getClient().notify(mDevice.getAddress(), UUIDUtils.makeUUID(0xFE95),
-                UUIDUtils.makeUUID(0x01), new BleNotifyResponse() {
+
+         //发送数据
+        String str =  "helloXX";
+
+        ClientManager.getClient().write(mDevice.getAddress(), UUIDUtils.makeUUID(serviceUuid),
+                UUIDUtils.makeUUID(charaUuid),str.getBytes(), new BleWriteResponse(){
+
+                    @Override
+                    public void onResponse(int code) {
+                        Log.e("tag", "write回调code：" + code);
+                    }
+                });
+
+        //接收数据
+        ClientManager.getClient().read(mDevice.getAddress(), UUIDUtils.makeUUID(serviceUuid),
+                UUIDUtils.makeUUID(charaUuid), new BleReadResponse(){
+
+
+
+
+                    @Override
+                    public void onResponse(int code, byte[] data) {
+                        Log.e("tag", "read接收code：" + code);
+                        if (data != null) {
+                            Log.e("tag", "接收数据成功：" + "data" + new String(data));
+                        }
+                    }
+                });
+
+        ClientManager.getClient().notify(mDevice.getAddress(), UUIDUtils.makeUUID(serviceUuid),
+                UUIDUtils.makeUUID(charaUuid), new BleNotifyResponse() {
                     @Override
                     public void onNotify(UUID service, UUID character, byte[] value) {
-
+                        Log.e("tag","onNotify:"+ "\n"+"service:"+service+ "\n"+"character"+character+"\n"+"value"+value);
                     }
 
                     @Override
                     public void onResponse(int code) {
-
+                        Log.e("tag","onResponse:"+code);
                     }
                 });
     }
